3d12af3f81ea6f3659ff9f542b19e41aee5f48fb
Ronaldo-Lemus <62957093+Ronaldo-Lemus@users.noreply.github.com>
2022-02-20 00:05:32 -0600

    Update agregar.php

--- dist/agregar.php
+++ dist/agregar.php
@@ -1,13 +1,13 @@
 <?php
 $productos=simplexml_load_file('productos.xml');
-$producto=$productos->addChild('materia');
+$producto=$productos->addChild('producto');
 $producto->addChild('codigo',$_POST['codigo']);
 $producto->addChild('nombre',$_POST['nombre']);
-$producto->addChild('Descripcion',$_POST['desu']);
-$producto->addChild('Imagenes',$_POST['imag']);
-$producto->addChild('Categoria',$_POST['categoria']);
-$producto->addChild('Precio',$_POST['precio']);
-$producto->addChild('Existencia',$_POST['exis']);
+$producto->addChild('descripcion',$_POST['desu']);
+$producto->addChild('img',$_POST['imag']);
+$producto->addChild('categoria',$_POST['categoria']);
+$producto->addChild('precio',$_POST['precio']);
+$producto->addChild('existencias',$_POST['exis']);
 
 file_put_contents('productos.xml',$productos->asXML());
 header('location:index.php?exito=1');
